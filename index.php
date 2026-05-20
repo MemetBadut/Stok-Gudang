365e2251064e4f2ff450641eff0a75da7793579c
MemetBadut <pasekm06@gmail.com>
2026-05-20 23:06:43 +0800

    halaman utama stok gudang

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Utama</title>
+</head>
+<body>
+    <table>
+        <th>
+            <td>KODE_BARANG</td>
+            <td>NAMA_BARANG</td>
+            <td>STOK_BARANG</td>
+            <td>NAMA_BARANG</td>
+            <td>ACTION</td>
+        </th>
+    </table>
+</body>
+</html>
