65fb38623a6946a7b2c900b2da47e91f64133cbb
MemetBadut <pasekm06@gmail.com>
2026-06-22 18:43:08 +0800

    feat: sidebar reusable

---
+++ includes/sidebar.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$navItems = require __DIR__ . '/../data/nav.php';
+
+$activeMenu = $activeMenu ?? '';
+$userName = $userName ?? 'Guest';
+$userEmail = $userEmail ?? 'guest@example.com';
+?>
+
+<aside class="inventory-sidebar">
+    
+</aside>
