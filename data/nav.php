0f9b7f329a8180082319e80a05bac749b05a0c83
MemetBadut <pasekm06@gmail.com>
2026-06-22 18:21:49 +0800

    feat: nav url

---
+++ data/nav.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    [
+        'key' => 'dashboard',
+        'label' => 'Dashboard',
+        'icon' => '',
+        'url' => url('admin/dashboard.php')
+    ],
+    [
+        'key' => 'barang',
+        'label' => 'Stok Barang',
+        'icon' => '',
+        'url' => url('pages/barang.php')
+    ],
+    [
+        'key' => 'login',
+        'label' => 'Login',
+        'icon' => '',
+        'url' => url('auth/login.php')
+    ],
+];
